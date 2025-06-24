72324b434713251ccbe0ce7c477fae9879786be7
sagar <sag333ar@gmail.com>
2025-06-24 06:03:11 +0530

    deployed version 1.0.0+0141

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3973728601"
+    serviceWorkerVersion: "946544416"
   }
 });
